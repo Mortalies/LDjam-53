ac3ff116132b27cc2517ad4f34b4329651060d3c
Anigilator322 <Anigilator322@gmail.com>
2023-05-01 18:30:32 +0700

    Fixed Merge

--- Assets/Scripts/SoulCharacters/SoulCharacter.cs
+++ Assets/Scripts/SoulCharacters/SoulCharacter.cs
@@ -1,28 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SoulCharacter : MonoBehaviour, IInteractable
 {
     private bool _isSinner;
     private TargetMovement _targetMovement;
     [SerializeField] private DialogueData _dialogueData;
-    public void Initialization(DialogueData dialogueData)
+
+    public UnityEvent soulMetHeaven = new UnityEvent();
+    public void Initialization(DialogueData dialogueData, bool isSinner)
     {
+        _isSinner = isSinner;
         _dialogueData = dialogueData;
     }
     public void Interact()
     {
         DialogueController.instance.SetDialogue(_dialogueData.messages);
-        DialogueController.instance.dialogueEndEvent.AddListener(AttachToTarget);
     }
-    public void AttachToTarget()
+
+    void Start()
     {
-        _targetMovement = GetComponent<TargetMovement>();
-        _targetMovement.ActivateMovement();
+
+    }
+    void Update()
+    {
+        
     }
     public void ApplyDamage()
     {
-        
+        if (_isSinner)
+            ScoreView.instance.UpdateScore();
+        gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<HeavenTrigger>() != null)
+        {
+            if (soulMetHeaven != null)
+            {
+                soulMetHeaven.Invoke();
+            }
+            HeavenTrigger heavenTrig = collision.gameObject.GetComponent<HeavenTrigger>();
+            heavenTrig.Triggered(_isSinner);
+            gameObject.SetActive(false);
+        }
     }
 }
