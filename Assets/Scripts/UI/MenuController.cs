fc389c16229dabcce9177bf272277183a0ad67ac
Lost_Shadow <69916626+Ra1mun@users.noreply.github.com>
2023-05-02 04:08:08 +0700

    123

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,22 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] private string _playSceneName;
     [SerializeField] private Animator _animator;
+    [SerializeField] private AudioSource _buttonClickSound;
+    [SerializeField] private AudioSource _backgroundMusic;
+    private bool isClicked = false;
     public void OnPlayButtonClick()
     {
         _animator.SetTrigger("Fade");
+        _buttonClickSound.Play();
+        isClicked = true;
     }
+
+    private void Update()
+    {
+        if (isClicked)
+            _backgroundMusic.volume = Mathf.MoveTowards(_backgroundMusic.volume, 0f, 0.125f);
+    }
+
     public void LoadScene()
     {
         SceneManager.LoadScene(_playSceneName);
